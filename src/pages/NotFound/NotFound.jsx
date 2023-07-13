29f8854fc653c1a2edf861ec9d77db0d92dfb90e
Nagateja Valiveti <teja91.valiveti@gmail.com>
2023-07-13 22:48:01 +0530

    Page not found

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import styles from "./NotFound.module.css";
+import Page404 from "../../assets/Page404.jpg";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
+
+  const { app__nopage, nopage__center, nopage__redirect } = styles;
+
   return (
-    <div>NotFound</div>
+    <main className={app__nopage}>
+      <div className={nopage__center}>
+        
+        <img src={Page404} alt="404" />
+
+        <div className={nopage__redirect}>
+          <h1>404</h1>
+          <p>Page Not Found!</p>
+          <Link to={'/'}>Back Home</Link>
+        </div>
+
+      </div>
+    </main>
   )
 }
 
